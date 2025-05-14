81ecb0a1c7340047d9bcd95a6b41bd5a2dc10288
Yibran Manuel <yibran.perve@gmail.com>
2025-05-14 10:41:46 -0600

    console log res.data4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ setShowMobileSidebar, onSearch }) => {
   const { user, isAuthenticated, logout } = useAuth();
   const [showModal, setShowModal] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  console.log("user de navbar",user.name);
+  console.log("user de navbar",user);
 
   const handleOpenModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
